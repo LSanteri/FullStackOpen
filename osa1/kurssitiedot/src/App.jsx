9dfcc5bb5d5ddad3419e98bbd6d063486d6b88c8
Santeri L <lallinsanteri0@gmail.com>
2024-09-03 01:33:14 +0300

    1.4 Osa tehty

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,22 +1,24 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
       <Header course={course} />
-      <Content pt1={part1} pt2={part2} pt3={part3}/>
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts}/>
+      <Total parts={parts} />
     </div>
   )
 }
@@ -32,32 +34,34 @@ const Header = (props) => {
   )
 }
 
-const Content = (parts) => {
+const Content = ({parts}) => {
+  console.log(parts)
   return (
     <div>
-      <Part name={parts.pt1.name} exercises={parts.pt1.exercises} />
-      <Part name={parts.pt2.name} exercises={parts.pt2.exercises} />
-      <Part name={parts.pt3.name} exercises={parts.pt3.exercises} />
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
+
     </div>
   )
 }
 
 
-const Total = (total) => {
-  console.log(total)
+const Total = ({parts}) => {
+  console.log(parts)
   return (
     <div>
       <p>
-        {total.total}
+        {parts[0].exercises + parts[1].exercises + parts[2].exercises}
       </p>
     </div>
   )
 }
 
-const Part = (parts) => {
+const Part = ({name, exercises}) => {
   return (
     <p>
-      {parts.name} {parts.exercises}
+      {name} {exercises}
     </p>
   )
 }
