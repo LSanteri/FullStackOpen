1d9ce9f76765dbbd4109b419eb8d3bc78faf86a1
Santeri L <lallinsanteri0@gmail.com>
2024-09-04 01:13:54 +0300

    Osa 1.11 tehty

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -5,7 +5,10 @@ const Button = ({onClick, text}) => (
 )
 
 const StatisticLine = ({text, value}) => (
-  <p>{text} {value}</p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
 
 const Statistics = (props) => {
@@ -19,12 +22,16 @@ const Statistics = (props) => {
  return (
   <div>
     <h2>Statistics</h2>
+    <table>
+      <tbody>
     <StatisticLine text="good" value={good}/>
     <StatisticLine text="neutral" value={neutral}/>
     <StatisticLine text="bad" value={bad}/>
     <StatisticLine text="total" value={total}/>
     <StatisticLine text="average" value={average}/>
     <StatisticLine text="positive" value={percentage}/>
+    </tbody>
+    </table>
   </div>
 )
 }
