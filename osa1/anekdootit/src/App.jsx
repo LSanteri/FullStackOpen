ddc28a00ec0c63262a3b8c4fc1525960bc913d9c
Santeri L <lallinsanteri0@gmail.com>
2024-09-04 01:27:10 +0300

    1.13 tehty

--- osa1/anekdootit/src/App.jsx
+++ osa1/anekdootit/src/App.jsx
@@ -10,22 +10,31 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
-  ];
+  ]
 
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anekdotes.length).fill(0))
 
   const handleNextAnekdote = () => {
-    const randomIndex = Math.floor(Math.random() * anekdotes.length);
+    const randomIndex = Math.floor(Math.random() * anekdotes.length)
     setSelected(randomIndex);
-  };
+  }
+
+  const handleVote = () => {
+    const votesCopy = [...votes]
+    votesCopy[selected] += 1;
+    setVotes(votesCopy)
+  }
 
   return (
     <div>
       <h1>Anekdote of the day</h1>
       <p>{anekdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVote}>Vote</button>
       <button onClick={handleNextAnekdote}>Next Anekdote</button>
     </div>
-  );
+  )
 }
 
 export default App;
