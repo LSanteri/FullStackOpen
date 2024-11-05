41c56ad246169e44a5429b4f00b676bf5446977b
Santeri L <lallinsanteri0@gmail.com>
2024-11-05 02:18:42 +0200

    Osa 2.16 tehty

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -3,12 +3,14 @@ import personService from './services/persons'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
+import Message from './components/Message'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [addMessage, setAddMessage] = useState()
 
   useEffect(() => {
     personService
@@ -19,41 +21,48 @@ const App = () => {
   }, [])
 
   const addNumber = (event) => {
-    event.preventDefault()
-    const personExists = persons.find((person) => person.name === newName)
-
+    event.preventDefault();
+    const personExists = persons.find((person) => person.name === newName);
+  
     personExists
-    ? window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)
-      ? personService
-          .update(personExists.id, { ...personExists, number: newNumber })
+      ? window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)
+        ? personService
+            .update(personExists.id, { ...personExists, number: newNumber })
+            .then((response) => {
+              setPersons(
+                persons.map((person) =>
+                  person.id !== personExists.id ? person : response
+                )
+              );
+              setAddMessage(`Updated ${newName}'s number`);
+              setTimeout(() => setAddMessage(null), 5000); // Clear message after 5 seconds
+              setNewName('');
+              setNewNumber('');
+            })
+        : null
+      : personService
+          .create({ name: newName, number: newNumber })
           .then((response) => {
-            setPersons(
-              persons.map((person) =>
-                person.id !== personExists.id ? person : response
-              )
-            )
-            setNewName('')
-            setNewNumber('')
-          })
-      : null // If the user cancels, do nothing
-    : personService
-          .create({ name: newName, number: newNumber })  
-          .then(response => {
-            setPersons(persons.concat(response))
-            setNewName('')
-            setNewNumber('')
-          })
-  }
-
+            setPersons(persons.concat(response));
+            setAddMessage(`Added ${newName}`);
+            setTimeout(() => setAddMessage(null), 5000); // Clear message after 5 seconds
+            setNewName('');
+            setNewNumber('');
+          });
+  };
+  
   const deleteIt = (id) => {
     if (window.confirm('Are you sure you want to delete this contact?')) {
       personService
         .remove(id)
         .then(() => {
-          setPersons(persons.filter(person => person.id !== id))
-        })
+          setPersons(persons.filter(person => person.id !== id));
+          setAddMessage('Deleted contact');
+          setTimeout(() => setAddMessage(null), 5000); // Clear message after 5 seconds
+        });
     }
-  }
+  };
+  
 
   const personsToShow = filter
     ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
@@ -78,6 +87,8 @@ const App = () => {
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
 
       <h3>Add a new</h3>
+
+      <Message message={addMessage}/>
 
       <PersonForm
         newName={newName}
